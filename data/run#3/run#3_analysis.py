17e6799e2ad7bac573215488f4218deb5f6e7f87
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-02-06 14:25:17 +0100

    Added script for run#3 in pandas

--- data/run#3/run#3_analysis.py
+++ data/run#3/run#3_analysis.py
@@ -9,6 +9,7 @@ www.nientepanico.org
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def reader(file):
     data = pd.read_csv(file, skiprows=6)
@@ -19,9 +20,39 @@ raw_data = reader("run#3-spreadsheet.csv")
 #Divido raw_data in due parti: data_head con info descrittive e data con i dati attuali
 data_head, data= raw_data[:19], raw_data[19:]
 
+#Creo una lista dei parametri di output
+out_parameter = data_head.loc[[12],"1":"1.13"]
+out_parameter.index = [0]
+
 #Elimino la prima colonna che non indica una mazza
 data = data.drop("[run number]", axis=1)
 
+#Cambio il nome delle righe
 new_index = np.arange(0,301)
+data.index = new_index
+
+#Funzione che calcola la media per colonna prende come argomenti il dataframe e il punto del parametro
+#Esempio, voglio la media di tutte le colonne del parametro .4 devo fare: mean(dataframe, ".4")
+def mean(dataframe, col=""):
+    mean_list = np.array([])
+    for a in range(1,1001):
+        column = dataframe[str(a)+col].to_numpy(dtype=np.float64)
+        mean = np.mean(column)
+        mean_list = np.append(mean_list,mean)
+    return mean_list
+
+#Funzione che calcola la deviazione standard
+def std(dataframe, col=""):
+    std_list = np.array([])
+    for a in range(1,1001):
+        column = dataframe[str(a)+col]
+        column=column.to_numpy(dtype=np.float64)
+        std = np.std(column)
+        std_list = np.append(std_list,std)
+    return std_list
+
+
+
+
 
 
